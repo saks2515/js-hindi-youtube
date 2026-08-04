a760bb6c44f72b64e08b2dd9268a69cd4140af63
Sakshi Kale <sakshikale1009@gmail.com>
2026-08-04 19:34:37 +0000

    Updated datatype.js

--- 01-basics/ConversionOperation.js
+++ 01-basics/ConversionOperation.js
@@ -1,26 +1,62 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber)  // NaN
+// console.log(valueInNumber)  // NaN
 
-// "33" => 33
-// "33abc"=> NaN
-// true => 1 or false => 0
+// // "33" => 33
+// // "33abc"=> NaN
+// // true => 1 or false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
  
-// "" => false
-// "sakshi"=> true
+// // "" => false
+// // "sakshi"=> true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+// *************** Operations ******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "sakshi"
+
+let str3 = str1 + " " + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
