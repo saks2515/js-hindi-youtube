5bec7a9b9a98c5940d0ab4b9d87c6acb819aef26
Sakshi Kale <sakshikale1009@gmail.com>
2026-08-05 18:25:26 +0000

    arrayss

--- 02-basics/2_array.js
+++ 02-basics/2_array.js
@@ -1,15 +1,15 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+ marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+ console.log(marvel_heros);
+ console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+// // const allHeros = marvel_heros.concat(dc_heros)
+// // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+//  const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
 
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+ console.log(Array.isArray("sakshi"))
+console.log(Array.from("sakshi"))
+ console.log(Array.from({name: "sakshi"})) // interesting     output is empty array
 
 let score1 = 100
 let score2 = 200
