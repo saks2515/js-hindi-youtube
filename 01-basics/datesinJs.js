528b43590288ccc551843261ad360fc10d344f3c
Sakshi Kale <sakshikale1009@gmail.com>
2026-08-05 17:19:56 +0000

    dates in js

--- 01-basics/datesinJs.js
+++ 01-basics/datesinJs.js
@@ -19,14 +19,14 @@
 // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
-//  let newDate = new Date()
-//  console.log(newDate);
-//  console.log(newDate.getMonth() + 1);
-//  console.log(newDate.getDay());
+  let newDate = new Date()
+  console.log(newDate);
+  console.log(newDate.getMonth() + 1);
+  console.log(newDate.getDay());
 
  //`${newDate.getDay()} and the time `
 
- newDate.toLocaleString('default', {
-     weekday: "long",
+//  newDate.toLocaleString('default', {
+//      weekday: "long",
     
- })
+//  })
